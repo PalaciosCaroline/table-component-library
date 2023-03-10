908852aeec27f91c56c281ee4ad4d96bf4ad8cd7
PalaciosCaroline <caroline2807@yahoo.fr>
2023-03-10 10:23:51 +0100

    Intallation répertoire du composant à publier

---
+++ src/dataForExemple.js
@@ -0,0 +1,51 @@
+const columnsExample = [
+    { label: 'First Name', property: 'firstName' },
+    { label: 'Last Name', property: 'lastName' },
+    { label: 'Start Date', property: 'startDate' },
+    { label: 'Department', property: 'department' },
+    { label: 'Date of Birth', property: 'dateOfBirth' },
+    { label: 'Street', property: 'street' },
+    { label: 'City', property: 'city' },
+    { label: 'State', property: 'state' },
+    { label: 'Zip Code', property: 'zipCode' },
+  ];
+
+const datasExample = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: '15/01/1975',
+      startDate: '01/04/2022',
+      department: 'Sales',
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'CA',
+      zipCode: '12345',
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      dateOfBirth: '17/05/1985',
+      startDate: '25/02/2020',
+      department: 'Marketing',
+      street: '456 Oak St',
+      city: 'Othertown',
+      state: 'NY',
+      zipCode: '67890',
+    },
+    {
+      firstName: 'Bob',
+      lastName: 'Johnson',
+      dateOfBirth: '30/09/1978',
+      startDate: '03/05/2019',
+      department: 'IT',
+      street: '789 Maple Ave',
+      city: 'Somewhere',
+      state: 'TX',
+      zipCode: '54321',
+    },
+  ];
+
+  export { datasExample, columnsExample };
+
+ 
---
+++ src/lib/components/Table.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { FaSortDown, FaSortUp } from 'react-icons/fa';
+
+export default function Table({ data, columns }) {
+  const [sortKey, setSortKey] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  const handleSort = (key, sortOrder) => {
+    setSortKey(key);
+    setSortOrder(sortOrder);
+  };
+
+  const sortDates = (a, b) => {
+    const dateRegex = /^\d{2}([./-])\d{2}\1\d{4}$/;
+    const isDate = dateRegex.test(a[sortKey]) && dateRegex.test(b[sortKey]);
+    if (isDate) {
+      const delimiter = dateRegex.exec(a[sortKey])[1];
+      const [dayA, monthA, yearA] = a[sortKey].split(delimiter).map((x) => parseInt(x, 10));
+      const [dayB, monthB, yearB] = b[sortKey].split(delimiter).map((x) => parseInt(x, 10));
+      const dateA = new Date(yearA, monthA - 1, dayA);
+      const dateB = new Date(yearB, monthB - 1, dayB);
+      if (dateA < dateB) return sortOrder === 'asc' ? -1 : 1;
+      if (dateA > dateB) return sortOrder === 'asc' ? 1 : -1;
+      return 0;
+    } else {
+      if (a[sortKey] < b[sortKey]) return sortOrder === 'asc' ? -1 : 1;
+      if (a[sortKey] > b[sortKey]) return sortOrder === 'asc' ? 1 : -1;
+      return 0;
+    }
+  };
+
+  const renderHeader = () => {
+    return (
+      <thead>
+        <tr>
+          {columnData.map(({ label, property, selectedBtnSort }) => (
+            <th key={property}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <p style={{ display: 'block', textAlign:'center'}}>{label}</p>
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => handleSort(property, 'asc')}
+                    style={{ marginLeft: '8px' , width: '50px',display: 'inline-block'}}
+                    className={selectedBtnSort && sortOrder === 'asc' ? 'selectedBtnSort' : ''}
+                  >
+                    <FaSortUp  style={{ height:'1.4rem', width: '1.4rem',verticalAlign: 'center' }} />
+                  </button>
+                
+                  <button
+                    type="button"
+                    onClick={() => handleSort(property, 'desc')}
+                    style={{ marginLeft: '8px', width: '50px',display: 'block', marginTop: '8px'}}
+                    className={selectedBtnSort && sortOrder === 'desc' ? 'selectedBtnSort' : ''}
+                  >
+                    <FaSortDown style={{ height:'1.4rem', width: '1.4rem',verticalAlign: 'center' }} />
+                  </button>
+                </div>
+              </div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  };
+
+  const renderBody = () => {
+    let sortedData = data;
+    if (sortKey !== null) {
+      sortedData = data.slice().sort((a, b) => {
+        if (typeof sortedData[0][sortKey] === 'string' && sortedData[0][sortKey].match(/^\d{2}([./-])\d{2}\1\d{4}$/)) {
+          return sortDates(a, b);
+        } else {
+          if (a[sortKey] < b[sortKey]) return sortOrder === 'asc' ? -1 : 1;
+          if (a[sortKey] > b[sortKey]) return sortOrder === 'asc' ? 1 : -1;
+          return 0;
+        }
+      });
+    }
+
+    return (
+      <tbody>
+        {sortedData.map((item, index) => (
+          <tr key={index}>
+            {columns.map(({ property }) => (
+              <td key={`cell-${index}-${property}`}>{item[property]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  const columnData = columns.map(({ label, property }) => ({
+    label,
+    property,
+    selectedBtnSort: sortKey === property,
+  }));
+
+  return (
+    <table>
+      {renderHeader(columnData)}
+      {renderBody()}
+    </table>
+  );
+}
