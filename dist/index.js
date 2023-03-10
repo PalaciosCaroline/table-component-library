72fa53b2aa4bf74004d9c25f87c46a8826dbc59f
PalaciosCaroline <caroline2807@yahoo.fr>
2023-03-10 13:33:30 +0100

    Mise à jour de mon package

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Table", {
+  enumerable: true,
+  get: function get() {
+    return _Table.default;
+  }
+});
+var _Table = _interopRequireDefault(require("./lib/components/Table"));
